a1ebfb68a0b88efc65bf96fcd5f27ac3a26adcc0
Hizkiasias <tesfayehizkiyas3@gmail.com>
2022-04-10 20:58:35 +0300

    m

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-[2;2R[3;1R[>77;30502;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000/**
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Holberton School");
+	printf("ALX acadamy");
 	return (0);
 }
