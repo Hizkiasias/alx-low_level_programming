8216dc7b1e7e8b5b15be88a7cba66cbe780905d5
Hizkiasias <tesfayehizkiyas3@gmail.com>
2022-04-17 20:27:27 +0300

    m

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,22 @@
-#include "holberton.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _isupper - a function that checks for uppercase character
- * @c: input
- * Return: 1 if c is uppercase, 0 otherwise
- */
-int _isupper(int c)
-{
-	if ((c >= 'A') && (c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month,int day);
+void print_remaining_days(int month,int day,int year);
+void print_to_98(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
